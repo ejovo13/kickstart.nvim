00bbe02e1b0527169182c04bd3224fd4d63afe59
Evan Voyles <evan.voyles@intrafentech.com>
2024-05-18 19:44:12 +0200

    Add skeleton for more modular config

---
+++ lua/user/snippets/snips.lua
@@ -0,0 +1,38 @@
+local ls = require 'luasnip'
+
+-- This is a snippet creator
+-- c(<trigger>, <nodes>)
+local s = ls.s
+
+-- This is a format node.
+-- It takes a format string and a list of nodes
+-- fmt(<fmt_string>, {...nodes})
+local fmt = require('luasnip.extras.fmt').fmt
+
+-- This is an insert node
+-- It takes a position (like $1) and optionally some default text
+-- i(<position>, [default_text])
+local i = ls.insert_node
+
+-- Repeats a node
+-- rep(<position>)
+local rep = require('luasnip.extras').rep
+
+ls.snippets = {
+  lua = {
+    -- Lua specific snippets go here.
+    s('req', fmt("local{} = require('{}')", { i(1, 'default'), rep(1) })),
+  },
+  python = {
+    s('req', fmt("local{} = require('{}')", { i(1, 'default'), rep(1) })),
+  },
+}
+
+ls.add_snippets('lua', {
+  -- Lua specific snippets go here.
+  s('req', fmt("local{} = require('{}')", { i(1, 'default'), rep(1) })),
+})
+
+ls.add_snippets('python', {
+  s('Settings', fmt('class {{}}(BaseSettings):\n    """Pydantic Settings """', {})),
+})
---
+++ lua/user/snippets/test.py
@@ -0,0 +1,5 @@
+from pydantic_settings import BaseSettings
+
+
+class MySettings(BaseSettings):
+    """Pydantic Settings"""
